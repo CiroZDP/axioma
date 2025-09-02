caa97ecfbb17c860f36eb29cc47e0245e44734ed
CiroZDP <cirod307@gmail.com>
2025-09-02 07:14:11 +0200

    chore: update tests (length)

--- src/main/java/org/axioma/Main.java
+++ src/main/java/org/axioma/Main.java
@@ -8,12 +8,15 @@ public class Main {
 
 	public static void main(String[] args) {
 		final float[] vec = new float[3]; // TODO: Vec3a.create
-		Vec3a.add(vec, 0, 1, 0);
-		Vec3a.add(vec, 0, 2f, 0);
-		Vec3a.sub(vec, 1, 3f, 0);
+		Vec3a.add(vec, 1, 1, 0);
+		
+		// TODO: 2. Inverse length
+		final float length = Vec3a.length(vec);
+		
+		// TODO: 1. div function
+		Vec3a.mul(vec, 1f / length, 1f / length, 1);
 		
 		System.out.println(Arrays.toString(vec));
-	
 	}
 	
 }
