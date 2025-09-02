dbd39cde728e3d7f912ca1d1bef363e9c0484935
CiroZDP <cirod307@gmail.com>
2025-09-02 09:11:54 +0200

    chore: rename methods

--- src/main/java/org/lwml/Main.java
+++ src/main/java/org/lwml/Main.java
@@ -6,8 +6,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		final Vec3 vec = new Vec3()
-				.translate(1, 2, 3)
-				.translate(1, 2, 3);
+				.add(1, 2, 3)
+				.add(1, 2, 3);
 		
 		System.out.println(vec);
 	
--- src/main/java/org/lwml/vec/Vec3.java
+++ src/main/java/org/lwml/vec/Vec3.java
@@ -1,27 +1,28 @@
 package org.lwml.vec;
 
+import java.nio.*;
 import java.util.Arrays;
 
 public final record Vec3(float[] c) {
 
 	public static final int X = 0, Y = 1, Z = 2;
-	
+
 	public Vec3() {
 		this(new float[3]);
 	}
-	
+
 	public Vec3(final float x, final float y, final float z) {
 		this(new float[3]);
 		this.set(x, y, z);
 	}
-	
+
 	public final Vec3 set(final float x, final float y, final float z) {
 		this.c[X] = x;
 		this.c[Y] = y;
 		this.c[Z] = z;
 		return this;
 	}
-	
+
 	public final Vec3 set(final Vec3 other) {
 		System.arraycopy(other.c, 0, this.c, 0, 3);
 		return this;
@@ -31,55 +32,75 @@ public final record Vec3(float[] c) {
 		Arrays.fill(c, 0);
 		return this;
 	}
-	
+
 	public final float x() {
 		return c[X];
 	}
-	
+
 	public final float y() {
 		return c[Y];
 	}
-	
+
 	public final float z() {
 		return c[Z];
 	}
-	
-	public final Vec3 _x(final float x) {
+
+	public final Vec3 x(final float x) {
 		this.c[X] = x;
 		return this;
 	}
-	
-	public final Vec3 _y(final float y) {
+
+	public final Vec3 y(final float y) {
 		this.c[Y] = y;
 		return this;
 	}
 
-	public final Vec3 _z(final float z) {
+	public final Vec3 z(final float z) {
 		this.c[Z] = z;
 		return this;
 	}
-	
-	public final Vec3 translate(final float tx, final float ty, final float tz, final Vec3 dst) {
-		dst._x(x() + tx);
-		dst._y(y() + ty);
-		dst._z(z() + tz);
+
+	public final Vec3 add(final float tx, final float ty, final float tz, final Vec3 dst) {
+		dst.c[X] = c[X] + tx;
+		dst.c[Y] = c[Y] + ty;
+		dst.c[Z] = c[Z] + tz;
 		return dst;
 	}
-	
-	public final Vec3 translate(final float tx, final float ty, final float tz) {
-		return translate(tx, ty, tz, this);
+
+	public final Vec3 add(final float tx, final float ty, final float tz) {
+		return add(tx, ty, tz, this);
 	}
-	
-	public final Vec3 translate(final Vec3 t, final Vec3 dst) {
-		return translate(t.x(), t.y(), t.z(), dst);
+
+	public final Vec3 add(final Vec3 t, final Vec3 dst) {
+		return add(t.x(), t.y(), t.z(), dst);
 	}
-	
-	public final Vec3 translate(final Vec3 t) {
-		return translate(t.x(), t.y(), t.z(), this);
+
+	public final Vec3 add(final Vec3 t) {
+		return add(t.x(), t.y(), t.z(), this);
 	}
-	
+
+	public final FloatBuffer toBuffer() {
+		final FloatBuffer buf = ByteBuffer.allocateDirect(12)
+				.order(ByteOrder.nativeOrder())
+				.asFloatBuffer()
+				.limit(3);
+		buf.put(c).rewind();
+		return buf;
+	}
+
+	@Override
 	public String toString() {
-		return Arrays.toString(c);
+		return "(" + x() + ", " + y() + ", " + z() + ")";
 	}
-	
+
+	@Override
+	public boolean equals(Object obj) {
+		return obj instanceof Vec3 v && Arrays.equals(this.c, v.c);
+	}
+
+	@Override
+	public int hashCode() {
+		return Arrays.hashCode(c);
+	}
+
 }
