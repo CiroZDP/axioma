9d5eb266ae03af97f26c959718948f92e5326a95
CiroZDP <cirod307@gmail.com>
2025-09-05 02:25:52 +0200

    feat: add base `Vec4`

--- src/main/java/org/lwml/Main.java
+++ src/main/java/org/lwml/Main.java
@@ -1,15 +1,22 @@
 package org.lwml;
 
-import org.lwml.vec.Vec3;
+import org.lwml.exp.Vec4;
 
 public class Main {
 
-	public static void main(String[] args) {
-		final Vec3 vec = Vec3.direction(1, 1, 0)
-				;
-		
-		System.out.println(vec);
-	
-	}
-	
+    public static void main(String[] args) {
+    	Vec4 vec = new Vec4();
+        bench("LWTK Vector Set Manual", () -> {
+        	vec.set(1, 2, 3, 4);
+        });
+    }
+
+    private static float bench(String name, Runnable task) {
+        long begin = System.nanoTime();
+        task.run();
+        long end = System.nanoTime();
+        float ms = (end - begin) * 1e-6f;
+        System.out.printf("%s%stook %.7f ms\n", name, " ".repeat(35 - name.length()), ms);
+        return ms;
+    }
 }
---
+++ src/main/java/org/lwml/exp/Vec4.java
@@ -0,0 +1,105 @@
+package org.lwml.exp;
+
+import java.lang.reflect.Field;
+import java.lang.ref.Cleaner;
+import sun.misc.Unsafe;
+
+@SuppressWarnings("removal")
+public final class Vec4 {
+
+	static final Unsafe UNSAFE;
+	static final Cleaner CLEANER = Cleaner.create();
+
+	public final long baseAddr, addr;
+	private final long padX, padY, padZ, padW;
+
+	static {
+		try {
+			Field f = Unsafe.class.getDeclaredField("theUnsafe");
+			f.setAccessible(true);
+			UNSAFE = (Unsafe) f.get(null);
+		} catch (Exception e) {
+			throw new UnsupportedOperationException("Unsafe not available", e);
+		}
+	}
+
+	public Vec4() {
+		this.baseAddr = UNSAFE.allocateMemory(16 + 15);
+		this.addr = (baseAddr + 15) & ~0xF;
+		{
+			this.padX = addr + 0;
+			this.padY = addr + 4;
+			this.padZ = addr + 8;
+			this.padW = addr + 12;
+		}
+
+		CLEANER.register(this, this::free);
+		
+		// Initialization
+		this.w(1.0f);		// Set 'W' component to 1.0f
+	}
+
+	/// -- Setters -- ///
+	public final Vec4 set(float[] arr) {
+		final long baseOffset = UNSAFE.arrayBaseOffset(float[].class);
+		UNSAFE.copyMemory(arr, baseOffset, null, addr, 16);
+		return this;
+	}
+	
+	public final Vec4 set(final float x, final float y, final float z, final float w) {
+		this.x(x);
+		this.y(y);
+		this.z(z);
+		this.w(w);
+		return this;
+	}
+
+	public final Vec4 zero() {
+		UNSAFE.setMemory(addr, 16, (byte) 0);
+		return this;
+	}
+
+	public final Vec4 x(final float x) {
+		UNSAFE.putFloat(padX, x);
+		return this;
+	}
+
+	public final Vec4 y(final float y) {
+		UNSAFE.putFloat(padY, y);
+		return this;
+	}
+
+	public final Vec4 z(final float z) {
+		UNSAFE.putFloat(padZ, z);
+		return this;
+	}
+
+	public final Vec4 w(final float w) {
+		UNSAFE.putFloat(padW, w);
+		return this;
+	}
+
+	/// -- Getters -- ///
+	
+	public final float x() {
+		return UNSAFE.getFloat(padX);
+	}
+
+	public final float y() {
+		return UNSAFE.getFloat(padY);
+	}
+
+	public final float z() {
+		return UNSAFE.getFloat(padZ);
+	}
+
+	public final float w() {
+		return UNSAFE.getFloat(padW);
+	}
+
+	/// -- Manual memory managment -- ///
+	
+	public final void free() {
+		UNSAFE.freeMemory(baseAddr);
+	}
+}
